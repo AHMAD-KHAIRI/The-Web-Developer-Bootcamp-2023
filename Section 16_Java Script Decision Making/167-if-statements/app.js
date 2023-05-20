03984f7ffe98ee00ac22974b276646d7a2ebb346
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2023-05-21 00:05:42 +0800

    added lecture 167 if statements

---
+++ Section 16_Java Script Decision Making/167-if-statements/app.js
@@ -0,0 +1,16 @@
+// console.log("It's working!");
+console.log("BEFORE CONDITIONAL");
+let random = Math.random();
+if (random < 0.5) {
+  //if (1 + 1 === 2) {
+  console.log("YOUR NUMBER IS LESS THAN 0.5!");
+  console.log(random);
+  //console.log("MATH STILL WORKS!");
+}
+if (random >= 0.5) {
+  //if (1 + 1 === 2) {
+  console.log("YOUR NUMBER IS GREATER THAN 0.5!");
+  console.log(random);
+  //console.log("MATH STILL WORKS!");
+}
+console.log("AFTER CONDITIONAL");
